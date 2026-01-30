0def4f4defd544c64c88efcc82c687ed9c0bc82a
Ryan <24RT0814@barton.ac.uk>
2026-01-30 10:00:52 +0000

    sillay

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTest26.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTest26.java
@@ -1,14 +1,40 @@
 package com.example.meepmeeptesting;
 
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-public class MeepMeepTest26 {
-    public void Main(String[] args)
-    {
+import org.jetbrains.annotations.NotNull;
 
+public class MeepMeepTest26 {
+    public static void main(String[] args)
+    {
+        System.setProperty("sun.java2d.opengl", "true");
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(56, 0, -Math.PI))
+                .waitSeconds(2)
+                        .lineToX(10)
+                .build()
+        );
+        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test26.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test26.java
@@ -4,35 +4,38 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Gyroscope;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 
 @TeleOp
 
 public class test26 extends LinearOpMode {
-    private Gyroscope imu;
-    private DcMotor motorR;
-    private DcMotor motorL;
-    private DcMotor HopperMotor;
-    private DcMotor Flywheel;
-    private Servo HopperServo;
-    private DigitalChannel digitalTouch;
-    private DistanceSensor sensorColorRange;
-
+//    private Gyroscope imu;
+//    private DcMotor motorR;
+//    private DcMotor motorL;
+//    private DcMotor HopperMotor;
+    private DcMotorEx Flywheel;
+//    private Servo HopperServo;
+//    private DigitalChannel digitalTouch;
+//    private DistanceSensor sensorColorRange;
+//
 
     @Override
     public void runOpMode() {
-        imu = hardwareMap.get(Gyroscope.class, "imu");
-        motorR = hardwareMap.get(DcMotor.class, "motorR");
-        motorL = hardwareMap.get(DcMotor.class, "motorL");
-        HopperMotor = hardwareMap.get(DcMotor.class, "HopperMotor");
-        Flywheel = hardwareMap.get(DcMotor.class, "Flywheel");
-        HopperServo = hardwareMap.get(Servo.class, "HopperServo");
+//        imu = hardwareMap.get(Gyroscope.class, "imu");
+//        motorR = hardwareMap.get(DcMotor.class, "motorR");
+//        motorL = hardwareMap.get(DcMotor.class, "motorL");
+//        HopperMotor = hardwareMap.get(DcMotor.class, "HopperMotor");
+        Flywheel = hardwareMap.get(DcMotorEx.class, "Flywheel");
+//        HopperServo = hardwareMap.get(Servo.class, "HopperServo");
         //digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
         //sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
 
@@ -40,44 +43,20 @@ public class test26 extends LinearOpMode {
         telemetry.update();
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
+        double P = 2.5;
+        double F = 0.5;
+
+        PIDFCoefficients pidfCoefficients = new PIDFCoefficients(P, 0.1, 0.2, F);
+        Flywheel.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
 
         // run until the end of the match (driver presses STOP)
-        double tgtPower = 0;
-        double tgtPowerx = 0;
-        boolean servoOpen = false;
-        while (opModeIsActive()) {
-            if (this.gamepad1.x) HopperMotor.setPower(-0.5);
-            else HopperMotor.setPower(0);
-            if (this.gamepad1.triangle)
-            {
-                if (servoOpen) {
-                    HopperServo.setPosition(1);
-                    servoOpen = false;
-                }
-                else {
-                    HopperServo.setPosition(0.5);
-                    servoOpen = true;
-                }
-            }
 
-            if (this.gamepad1.right_trigger != 0)
-            {
-                Flywheel.setPower(1);
-                sleep(1000);
-                Flywheel.setPower(0);
-            }
-            if (this.gamepad1.right_stick_button) HopperServo.setPosition(0.5);
-            tgtPower = -this.gamepad1.left_stick_y;
-            tgtPowerx = -this.gamepad1.left_stick_x;
-            motorR.setPower(tgtPower + tgtPowerx);
-            motorL.setPower(-tgtPower + tgtPowerx);
-            telemetry.addData("Target Power", tgtPower);
-            telemetry.addData("Motor Power", motorR.getPower());
-            telemetry.addData("Servo position", HopperServo.getPosition());
-            telemetry.addData("Servo open", servoOpen);
-            telemetry.addData("Status", "Running");
-            telemetry.addData("IMU", imu.getAngularVelocity(AngleUnit.RADIANS));
-            telemetry.addLine("press triangle to wave, right trigger for flywheel, press square to spin the feeder. left stick to move and pressing the right stick ");
+        while (opModeIsActive()) {
+           Flywheel.setVelocity((gamepad1.right_trigger - gamepad1.left_trigger)*1800);
+
+
+            telemetry.addData("Current position", Flywheel.getCurrentPosition());
+
             telemetry.update();
         }
     }
