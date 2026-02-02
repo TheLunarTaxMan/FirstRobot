b74f9f27a8708799db839f2325eb2e2f27fa4957
Ryan <24RT0814@barton.ac.uk>
2026-02-02 14:10:28 +0000

    sillay

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTest26.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTest26.java
@@ -3,6 +3,7 @@ package com.example.meepmeeptesting;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Rotation2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
@@ -26,9 +27,17 @@ public class MeepMeepTest26 {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(56, 0, -Math.PI))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(62, 12, -Math.PI))
                 .waitSeconds(2)
-                        .lineToX(10)
+                .splineTo(new Vector2d(-35,35), 15)
+                .turnTo(3*Math.PI/4)
+
+                        .waitSeconds(5)
+
+                        .splineTo(new Vector2d(56, 54), -5)
+                        .turnTo(-Math.PI)
+                        .strafeTo(new Vector2d(57, 57))
+                        .waitSeconds(5)
                 .build()
         );
         meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_DARK)
