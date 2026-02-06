55297f1d27a9da437c7ec958a86734205b61cc38
Ryan <24RT0814@barton.ac.uk>
2026-02-06 09:58:51 +0000

    sillay

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/AutoSmallR26.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/AutoSmallR26.java
@@ -1,50 +1,32 @@
 package com.example.meepmeeptesting;
 
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.acmerobotics.roadrunner.Action;
-import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.Rotation2d;
-import com.acmerobotics.roadrunner.Vector2d;
-import com.noahbres.meepmeep.MeepMeep;
-import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
-import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-import org.jetbrains.annotations.NotNull;
-
-public class MeepMeepTest26 {
+public class AutoSmallR26 {
     public static void main(String[] args)
     {
         System.setProperty("sun.java2d.opengl", "true");
         MeepMeep meepMeep = new MeepMeep(800);
-
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(62, 12, -Math.PI))
-                .waitSeconds(2)
-//                .splineTo(new Vector2d(-35,35), 15)
-//                .turnTo(3*Math.PI/4)
-                            //   .splineToConstantHeading(new Vector2d(-35,35), 3*Math.PI/4)
+            .waitSeconds(2)
                         .splineToLinearHeading(new Pose2d(-35,35, 3*Math.PI/4), 3*Math.PI/4)
-                //shoot
-                        .waitSeconds(5)
+            //shoot
+            .waitSeconds(5)
                         .strafeToLinearHeading(new Vector2d(-11, 20), Math.PI/2)
-//                        .splineToLinearHeading(new Pose2d(-11, 20, Math.PI/2), -1)
-                //reload
-                        .strafeTo(new Vector2d(-11, 52))
-//                        .lineToY(52)
-//                        .lineToX(-11)
-
-                .strafeToLinearHeading(new Vector2d(-35, 35), 3*Math.PI/4)
-                //shoot again
-                .build()
+            //reload
+            .strafeTo(new Vector2d(-11, 52))
+            .strafeToLinearHeading(new Vector2d(-35, 35), 3*Math.PI/4)
+            //shoot again
+            .build()
         );
         meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_DARK)
                 .setDarkMode(true)
