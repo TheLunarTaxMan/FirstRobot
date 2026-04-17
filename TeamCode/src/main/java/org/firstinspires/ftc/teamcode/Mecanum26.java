2cc6d7f0b35adf016ff8980a1cf07f65bd5faa0d
Ben <bencspratt@gmail.com>
2026-04-17 08:51:59 +0100

    studd

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum26.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum26.java
@@ -6,9 +6,52 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+//import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp
+
+
+
 public class Mecanum26 extends LinearOpMode {
+ /*   public class Motor26{
+        DcMotorEx motor;
+        double Ki;
+        double Kd;
+        double Kp;
+
+        double reference;
+
+        double integralSum = 0;
+
+        double lastError = 0;
+        ElapsedTime timer = new ElapsedTime();
+        Motor26(DcMotorEx Inmotor){
+            motor = Inmotor;
+        }
+
+        void loop(){
+            // obtain the encoder position
+            double encoderPosition = motor.getVelocity();
+            // calculate the error
+            double error = reference - encoderPosition;
+
+            // rate of change of the error
+            double derivative = (error - lastError) / timer.seconds();
+
+            // sum of all error over time
+            integralSum = integralSum + (error * timer.seconds());
+
+            double out = (Kp * error) + (Ki * integralSum) + (Kd * derivative);
+
+            motor.setPower(out);
+
+            lastError = error;
+
+            // reset the timer for next time
+            timer.reset();
+        }
+    }*/
     public DcMotorEx frontRightWheel;
     public DcMotorEx frontLeftWheel;
     public DcMotorEx backRightWheel;
@@ -24,9 +67,13 @@ public class Mecanum26 extends LinearOpMode {
     public void runOpMode()
     {
         frontRightWheel = hardwareMap.get(DcMotorEx.class, "rightFront");
+        frontRightWheel.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
         frontLeftWheel = hardwareMap.get(DcMotorEx.class, "leftFront");
+        frontLeftWheel.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
         backRightWheel = hardwareMap.get(DcMotorEx.class, "rightBack");
+        backRightWheel.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
         backLeftWheel = hardwareMap.get(DcMotorEx.class, "leftBack");
+        backLeftWheel.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
         HopperMotor = hardwareMap.get(DcMotor.class, "HopperMotor");
         HopperServo = hardwareMap.get(Servo.class, "HopperServo");
         Flywheel = hardwareMap.get(DcMotorEx.class, "Flywheel");
@@ -38,35 +85,34 @@ public class Mecanum26 extends LinearOpMode {
         backLeftWheel.setDirection(DcMotorSimple.Direction.REVERSE);
         frontLeftWheel.setDirection(DcMotorSimple.Direction.REVERSE);
         boolean HopperMotorOn= false;
-        boolean HopperServoUp = false;
         boolean fast = true;
+        ElapsedTime ArmTimer = new ElapsedTime();
         while (opModeIsActive())
         {
             Forward = this.gamepad1.left_stick_y;
             Strafe = this.gamepad1.left_stick_x;
             Rotation = -this.gamepad1.right_stick_x;
-            /*if (this.gamepad1.a){
-                frontLeftWheel.setPower(0.4);
+            if (this.gamepad1.a){
+                frontLeftWheel.setVelocity(-1000);
             } else if (this.gamepad1.x) {
-                frontRightWheel.setPower(0.4);
+                frontRightWheel.setVelocity(-1000);
             }else if (this.gamepad1.y){
-                backRightWheel.setPower(0.4);
+                backRightWheel.setVelocity(-1000);
             }else if (this.gamepad1.b){
-                backLeftWheel.setPower(0.4);
-            }else {*/
-                frontRightWheel.setVelocity((Forward - Rotation - Strafe)*2800);
-                backRightWheel.setVelocity((Forward - Rotation + Strafe)*2800*2);
-                frontLeftWheel.setVelocity((Forward + Rotation + Strafe)*2800);
-                backLeftWheel.setVelocity((Forward + Rotation - Strafe)*2800*2);
-            //}
-            if (this.gamepad1.rightBumperWasPressed()){
-                if (HopperServoUp) {
-                    HopperServo.setPosition(1);
-                }else{
-                    HopperServo.setPosition(0.8);
-                }
-                HopperServoUp = !HopperServoUp;
+                backLeftWheel.setVelocity(-1000);
+            }else {
+            frontRightWheel.setVelocity((Forward - Rotation - (Strafe*1))*2800);
+            backRightWheel.setVelocity((Forward - Rotation + (Strafe*1))*2800);
+            frontLeftWheel.setVelocity((Forward + Rotation + (Strafe*1))*2800);
+            backLeftWheel.setVelocity((Forward + Rotation - (Strafe*1))*2800);
+            }
 
+            if (this.gamepad1.rightBumperWasPressed()){
+                HopperServo.setPosition(0.8);
+                ArmTimer.reset();
+            }
+            if (ArmTimer.milliseconds() > 800){
+                HopperServo.setPosition(1);
             }
             Flywheel.setPower((gamepad1.right_trigger - gamepad1.left_trigger));
 /*
@@ -77,14 +123,14 @@ public class Mecanum26 extends LinearOpMode {
             else {
                 Flywheel.setVelocity((gamepad1.right_trigger - gamepad1.left_trigger) * 3000);
             }
-            if (gamepad1.rightBumperWasPressed())
+            if (gamepad1.leftBumperWasPressed())
             {
                 fast = !fast;
             }*/
 
             if (this.gamepad1.dpadDownWasPressed())
             {
-                TruffleShuffle();
+              //  TruffleShuffle();
             }
 
             if(this.gamepad1.leftBumperWasPressed()){
@@ -97,13 +143,17 @@ public class Mecanum26 extends LinearOpMode {
                 }
             }
 
+            telemetry.addData("FWD", Forward);
+            telemetry.addData("STR", Strafe);
+            telemetry.addData("ROT", Rotation);
+            telemetry.addData("is fast?",fast);
+            telemetry.addData("BLS", backLeftWheel.getVelocity());
+            telemetry.addData("BRS", backRightWheel.getVelocity());
+            telemetry.addData("FLS", frontLeftWheel.getVelocity());
+            telemetry.addData("FRS", frontRightWheel.getVelocity());
             telemetry.addData("Left stick for axis movement, right stick for rotation", ' ');
-            telemetry.addData("Left bumper to toggle the feed in motor", ' ');
-            telemetry.addData("Right bumper to toggle the hopper servo", ' ');
-            telemetry.addData("Right trigger to spin up the flywheel (HOLD)", ' ');
-            telemetry.addData("Down on the d-pad to get the ball from the feeder (truffle shuffle)", ' ');
-            telemetry.addData("Hopper servo is at: " , HopperServo.getPosition());
-
+            telemetry.addData("Hopper servo" , HopperServo.getPosition());
+            updateTelemetry(telemetry);
         }
 
     }
