d828fbc2c7ad4d46bf9afabda9684e48715bc7f6
Ryan <24RT0814@barton.ac.uk>
2026-04-21 06:23:05 -0700

    yes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunnerTest26.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunnerTest26.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
@@ -12,42 +15,128 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class RoadRunnerTest26 extends LinearOpMode {
-    private DcMotor HopperMotor;
+    private DcMotorEx Flywheel;
+    private DcMotorEx HopperMotor;
+    private Servo HopperServo;
     private DigitalChannel digitalTouch;
     private DistanceSensor sensorColorRange;
-    private Servo servoTest;
-
-    public class FlyWheel {
 
 
+    public  class FlyWheelclass {
+        int flywheelspeed = 850;
+
+        public FlyWheelclass(){
+
+        }
+        public Action shootThree() { return new ShootThree(); }
+        public class ShootThree implements Action {
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+
+                Flywheel.setVelocity(flywheelspeed);
+                HopperServo.setPosition(1);
+                HopperMotor.setPower(0);
+                sleep(2000);
+                HopperServo.setPosition(0.8);
+                sleep(800);
+                HopperServo.setPosition(1);
+                sleep(1000);
+
+                HopperServo.setPosition(0.8);
+                sleep(600);
+                HopperServo.setPosition(1);
+                sleep(1000);
+
+                HopperMotor.setPower(-1);
+                sleep(300);
+                HopperMotor.setPower(1);
+                sleep(100);
+                HopperServo.setPosition(0.8);
+                sleep(600);
+                HopperServo.setPosition(1);
+                sleep(1000);
+
+                Flywheel.setVelocity(0);
+                HopperMotor.setPower(0);
+                HopperServo.setPosition(1);
+
+                return(true);
+            }
+        }
+
+
+        public void TestyThing()
+        {
+
+            Flywheel.setVelocity(flywheelspeed);
+            HopperServo.setPosition(1);
+            HopperMotor.setPower(0);
+            sleep(2000);
+            HopperServo.setPosition(0.8);
+            sleep(800);
+            HopperServo.setPosition(1);
+            sleep(1000);
+
+            HopperServo.setPosition(0.8);
+            sleep(600);
+            HopperServo.setPosition(1);
+            sleep(1000);
+
+            HopperMotor.setPower(-1);
+            sleep(300);
+            HopperMotor.setPower(1);
+            sleep(100);
+            HopperServo.setPosition(0.8);
+            sleep(600);
+            HopperServo.setPosition(1);
+            sleep(1000);
+
+            Flywheel.setVelocity(0);
+            HopperMotor.setPower(0);
+            HopperServo.setPosition(1);
+
+
+        }
     }
 
     public void runOpMode() {
 
         Pose2d initialPos = new Pose2d(0, 0, Math.toRadians(90));
         MecanumDrive drive = new MecanumDrive(hardwareMap, initialPos);
+        Flywheel = hardwareMap.get(DcMotorEx.class, "Flywheel");
+        HopperMotor = hardwareMap.get(DcMotorEx.class, "HopperMotor");
+        HopperServo = hardwareMap.get(Servo.class, "HopperServo");
 
-        TrajectoryActionBuilder intialTrajectory = drive.actionBuilder(initialPos)
-                .waitSeconds(2)
-                .lineToX(50)
-                .strafeTo(new Vector2d(10,30))
-                .strafeToLinearHeading(new Vector2d(-20,-30),-Math.PI/2)
-                .splineTo(new Vector2d(20,10),2);
-
+        FlyWheelclass fw = new FlyWheelclass();
+        waitForStart();
         Action movement = drive.actionBuilder(initialPos)
-                .strafeTo(new Vector2d(0,0))
+                .waitSeconds(2)
+                .splineToLinearHeading(new Pose2d(-12,-12, -3*Math.PI/4), -3*Math.PI/4)
+                .build();
+
+        Action moveagain =  drive.actionBuilder(initialPos)
+                //shoot
+                .waitSeconds(5)
+                .strafeToLinearHeading(new Vector2d(-11, -20), -Math.PI/2)
+                //reload
+                .strafeTo(new Vector2d(-11, -32))
+//                        .strafeToLinearHeading(new Vector2d(-35, -35), -3*Math.PI/4)
+                //shoot again
                 .build();
 
         Actions.runBlocking(
                 new SequentialAction(
                         movement
-                        
+                        ,fw.shootThree()
+                        ,moveagain
                 ));
     }
 }
